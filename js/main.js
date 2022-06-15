9c2d26a068a413109991daf6ec128edab2b6748a
alanBonnet <84151943+alanBonnet@users.noreply.github.com>
2022-06-15 16:37:02 -0300

    Agregado de botones

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let botonRegistrarse = document.getElementById('login')
+let botonIngresar = document.getElementById('singin')
+let canSeeReg= false;
+let canSeeLog=false;
+function registrarDo(){
+    canSeeReg = !canSeeLog
+    if()
+}
