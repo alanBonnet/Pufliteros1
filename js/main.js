14cb68e95fa4e36d6f6543c2be399cc0352f208a
alanBonnet <84151943+alanBonnet@users.noreply.github.com>
2022-06-16 16:04:26 -0300

    agregado de funciones de botones

--- js/main.js
+++ js/main.js
@@ -1,8 +1,37 @@
 let botonRegistrarse = document.getElementById('login')
 let botonIngresar = document.getElementById('singin')
+let botonera = document.getElementById('botonera')
+let registerForm = document.getElementById('registerForm')
+let loginForm = document.getElementById('loginForm')
+
+
 let canSeeReg= false;
 let canSeeLog=false;
 function registrarDo(){
-    canSeeReg = !canSeeLog
-    //if()
+    
+    
+    canSeeReg = !canSeeReg
+    if (canSeeReg){
+        registerForm.hidden = false
+        botonera.hidden=true
+    }else{
+        registerForm.hidden = true
+        botonera.hidden= false
+    }
+    
+    
+}
+function ingresarDo(){
+    
+    
+    canSeeReg = !canSeeReg
+    if (canSeeReg){
+        loginForm.hidden = false
+        botonera.hidden=true
+    }else{
+        loginForm.hidden = true
+        botonera.hidden= false
+    }
+    
+    
 }
