060a119bf9a9e6ff45fec363f3c1cb8fec2577b6
alanBonnet <84151943+alanBonnet@users.noreply.github.com>
2022-06-27 14:56:41 -0300

    php prueba

--- controllers/tareas/guardarTarea.php
+++ controllers/tareas/guardarTarea.php
@@ -2,20 +2,34 @@
     include("../../core/db.php");
 
     if(isset($_POST['save_task'])){
-        $title = $_POST['title'];
-        $description = $_POST['description'];
-        
-
-        $tarea = $conexion->prepare("INSERT INTO tareas(nombre, descripcion) VALUES (':nombre',':descripcion')");
-        $tarea->bindParam(':nombre',$title);
-        $tarea->bindParam(':descripcion',$description);
-        $tarea->execute();
-
-        $contador= $tarea->rowCount();
-        if($contador == 1){
-            echo "creado";
-        }else{
-            echo "no se pudo pa";
+        try{
+            $conexion->beginTransaction();
+            $title = $_POST['title'];
+            $description = $_POST['description'];
+            
+            $sql = "INSERT INTO tareas(nombre, descripcion) VALUES (':nombre',':descripcion')";
+            $tarea = $conexion->prepare($sql);
+            // $tarea->bindParam(':nombre',$title);
+            // $tarea->bindParam(':descripcion',$description);
+            $tarea->execute(array(':nombre'=>$title,
+                                ':descripcion'=>$description));
+                
+            $contador= $tarea->rowCount();
+            $fecha = date_create();
+            $zonaHoraria = new DateTimeZone('-0300') ;
+            $fecha->setTimezone($zonaHoraria);
+            print_r($fecha);
+            
+            if($contador == 1){
+                $conexion->commit();
+                echo "Usuario ingresado correctamente";
+            }else{
+                $conexion->rollBack();
+                echo "El usuario no pudo ser ingresado, intente mas tarde";
+            }
+        }
+        catch(\Throwable $th){
+            echo $th -> getMessage()." valiste pija";
         }
         // $result = mysqli_query($conexion,$query);
         // if(!$result){
