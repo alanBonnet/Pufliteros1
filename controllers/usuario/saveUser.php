2ae308ebd18fb8febfc7938afb3b09cc3b6b4488
alanBonnet <84151943+alanBonnet@users.noreply.github.com>
2022-06-29 14:22:34 -0300

    sumado de consulta insert

--- controllers/usuario/saveUser.php
+++ controllers/usuario/saveUser.php
@@ -10,8 +10,17 @@
         if( strlen($pass)>=8){
             if($pass == $passConfirm){
                 if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-                    
-                    
+                    $sql = "INSERT INTO usuarios VALUES(':nombre',':email',':pass')";
+                    $nuevoUsuario = $conexion->prepare($sql);
+                    $nuevoUsuario->bindParam(':nombre',$nombre);
+                    $nuevoUsuario->bindParam(':email',$email);
+                    $nuevoUsuario->bindParam(':password',$password);
+                    $nuevoUsuario->execute();
+                    $count = $nuevoUsuario->rowCount();
+                    if ($count == 1){
+                        $titulo = "Bienvenido".$nombre;
+                        
+                    }
                 }else{
                     echo " email no válido ";
                 }
