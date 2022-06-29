3a131401aee18324995564dcca197b8536989647
LautaDurante <107212734+LautaDurante@users.noreply.github.com>
2022-06-29 12:57:41 -0300

    hola jajajajaa

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,14 @@
 <!-- Footer-->
 <footer class="py-5 bg-black">
-            <div class="container px-5"><p class="m-0 text-center text-white small">Copyright &copy; Your Website 2022</p></div>
+            <div class="container px-5"><p class="m-0 text-center text-white small">Copyright &copy; TCAD </p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Scroll animate -->
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        <script>
+          AOS.init();
+        </script>
         <!-- Core theme JS-->
 <script src="../NewModel/js/scripts.js"></script>
 <!-- <script src="../js/main.js"></script> -->
