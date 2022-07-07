d2f046ff1a8038a72661292c03031daf18776876
LautaDurante <107212734+LautaDurante@users.noreply.github.com>
2022-07-07 13:14:15 -0300

    curriculummm

--- includes/showCurriculum.php
+++ includes/showCurriculum.php
@@ -1,15 +1,94 @@
 <?php include('./header.php');session_start()?>
 <?php include('./navbar.php')?>
+
 <div class="container mt-5 pt-5" style="min-height:calc(100vh - 100px);">
     <div class="row">
-        <div class="col-12"><?php echo $_SESSION['curriculum']['nombre_y_apellido'] ?></div>
-        <div class="col-12"><?php echo $_SESSION['curriculum']['trabajo_interes'] ?></div>
-        <div class="col-12"><?php echo $_SESSION['curriculum']['email_curriculum'] ?></div>
-        <div class="col-12"><?php echo $_SESSION['curriculum']['nro_telefono'] ?></div>
-        <div class="col-12"><?php echo $_SESSION['curriculum']['experiencia'] ?></div>
-        <div class="col-12"><?php echo $_SESSION['curriculum']['intereses'] ?></div>
-        <div class="col-12"><?php echo $_SESSION['curriculum']['educacion_calificacion'] ?></div>
-        <div class="col-12"><?php echo $_SESSION['curriculum']['habilidades'] ?></div>
+
+    <body class="bg-light">
+        <div class="container p-5">
+            <div class="row">
+                <div class="col-lg-4 bg-dark text-white text-center py-4">
+                    <div class="header-left">
+                        <img src="/NewModel/assets/img/2da img.jpg" alt="" class="img-thumbnail rounded-circle mb-2">
+                        <h1 class="display-5">Lautaro Durante</h1>
+                        <h4 class="lead text-uppercase text-white-50 nb-4">Programador</h4>
+                    </div>
+
+                    <div>
+                        <h5 class="text-uppercase bg-white text-dark py-2 rounded-pill">Contacto</h5>
+
+                        <ul class="list-unstyled text-white-50 ml-5 py-2 text-left">
+                            <li class="list-item">
+                                <i class="fas fa-mobile-alt mx-4"></i>
+                                +3704 784512
+                            </li>
+                            <li class="list-item">
+                                <i class="fas fa-envelope-open-text mx-4"></i>
+                                mail@gmail.com
+                            </li>
+                            <li class="list-item">
+                                <i class="fas fa-blog mx-4"></i>
+                                wwww.ejemplo.com
+                            </li>
+                            <li class="list-item">
+                                <i class="fas fa-map-marker-alt mx-4"></i>
+                                Formosa, Ciudad
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div>
+                    <h5 class="text-uppercase bg-white text-dark py-2 rounded-pill">Habilidades</h5> 
+                    <ul class="list text-white-50 ml-5 py-2 text-left text-capitalize">
+                        <li class="list-item">Habilidades para resolver el problema</li>
+                        <li class="list-item">Comunicacion verbal</li>
+                        <li class="list-item">Servicio al cliente</li>
+                    </ul>
+                    </div>
+
+                    <div>
+                    <h5 class="text-uppercase bg-white text-dark py-2 rounded-pill">Educación</h5> 
+                    <ul class="list text-white-50 ml-5 py-2 text-left text-capitalize">
+                        <li class="list-item">Colegio Secundario Don Bosco</li>
+                        <li class="list-item">Colegio Secundario Don Bosco</li>
+                        <li class="list-item">Colegio Secundario Don Bosco</li>
+                    </ul>
+                    </div>
+                </div>
+                <div class="col-lg-8 bg-light text-dark py-5 px-5">
+                    <div class="head-right">
+                        <h4 class="text-uppercase">Perfil</h4>
+                        <hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi debitis ducimus nesciunt. Magni nesciunt perspiciatis porro eligendi quos. Enim quidem quod saepe ipsam beatae eum dolor. Enim tenetur culpa libero earum quasi amet praesentium architecto aliquam asperiores molestias esse reiciendis modi sint, error sequi beatae harum necessitatibus possimus cumque quis?</p>
+                    </div>
+
+                    <div>
+                    <h4 class="text-uppercase">Experiencia en el trabajo</h4>
+                    <hr>
+                    
+                    <ul class="list">
+                        <li class="list-item">
+                            <h5 class="display-6 text-uppercase">Marketing en linea</h5>
+                            <h6 class="text-uppercase text-black-50">nombre de la comapia / ejemplo</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita tenetur repellat quae at magnam distinctio quidem eius explicabo pariatur ipsum asperiores dicta natus corrupti exercitationem, numquam nemo voluptates rem qui doloremque molestias in, quasi sunt! Asperiores officia architecto maxime enim vel rem accusamus quidem, beatae itaque maiores. Aperiam, exercitationem repellendus?</p>
+                        </li>
+                        <li class="list-item">
+                            <h5 class="display-6 text-uppercase">Marketing en linea</h5>
+                            <h6 class="text-uppercase text-black-50">nombre de la comapia / ejemplo</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita tenetur repellat quae at magnam distinctio quidem eius explicabo pariatur ipsum asperiores dicta natus corrupti exercitationem, numquam nemo voluptates rem qui doloremque molestias in, quasi sunt! Asperiores officia architecto maxime enim vel rem accusamus quidem, beatae itaque maiores. Aperiam, exercitationem repellendus?</p>
+                        </li>
+                        <li class="list-item">
+                            <h5 class="display-6 text-uppercase">Marketing en linea</h5>
+                            <h6 class="text-uppercase text-black-50">nombre de la comapia / ejemplo</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita tenetur repellat quae at magnam distinctio quidem eius explicabo pariatur ipsum asperiores dicta natus corrupti exercitationem, numquam nemo voluptates rem qui doloremque molestias in, quasi sunt! Asperiores officia architecto maxime enim vel rem accusamus quidem, beatae itaque maiores. Aperiam, exercitationem repellendus?</p>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </body>
 
     </div>
 </div>
